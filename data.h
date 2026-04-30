db23dddd428f79faf057b8af0c9ff66f85204782
aiddao <2ndtry91@gmail.com>
2026-04-30 10:13:49 -0700

    Made Question and QuestionList class with method headers

---
+++ data.h
@@ -0,0 +1,32 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Question {
+    private:
+        string question;
+        string first_hint;
+        string second_hint;
+    public:
+        Question(string q, string f, string s) : question(q), first_hint(f), second_hint(s) {}
+        string getQuestion();
+        string getFirstHint();
+        string getSecondHint();
+};
+
+class QuestionList {
+    private:
+        vector<Question> questions;
+    public:
+        QuestionList();
+        bool load_questions(const string& filename);
+        const vector<Question>& get_questions() const;
+        const vector<Question>& get_questions(int index) const;
+        void display_questions() const;
+};
+
+#endif
