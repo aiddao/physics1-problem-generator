725ca252baf868d4f22c0eebbfda166ba1e891f3
aiddao <2ndtry91@gmail.com>
2026-04-30 10:53:51 -0700

    Added functions in main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
+#include "data.h"
+
 int main(int argc, char *argv[]){
     //load questions
+    QuestionList list;
+    if(!list.load_questions("questions.json")){
+        return 1;
+    }
+    list.display_questions();
     //check if questions are loaded
         // if not return 1
     //start main operation
